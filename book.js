d25e1587cf2ea84fee734ffea11356a08709ccd2
FKeir <fmkeir@gmail.com>
2020-04-19 21:35:56 +0100

    Refactor Book,Library to uuse ES6 class notation and import/export statements

--- book.js
+++ book.js
@@ -1,7 +1,9 @@
-const Book = function (author, title, genre) {
+class Book {
+  constructor(author, title, genre) {
   this.author = author;
   this.title = title;
   this.genre = genre;
-};
+}
+}
 
-module.exports = Book;
+export default Book;
